9d995aa69e98afe3c241a7b58221fb9936c9fd62
chopix <fresserslegend@gmail.com>
2024-04-21 14:18:37 +0300

    asd

--- bot.js
+++ bot.js
@@ -1,126 +1,135 @@
-import {Markup, session, Telegraf} from "telegraf";
+import { Markup, session, Telegraf } from 'telegraf'
 import 'dotenv/config'
-import start from "./commands/start.js";
-import restart from "./commands/restart.js";
+import start from './commands/start.js'
+import restart from './commands/restart.js'
 import circle from './commands/circle.js'
 import gpt from './commands/gpt.js'
-import {Stage} from "telegraf/scenes";
-import {gptScene} from "./scene/gpt.js";
-import {sequelize} from "./config/sequelize.js";
+import { Stage } from 'telegraf/scenes'
+import { gptScene } from './scene/gpt.js'
+import { sequelize } from './config/sequelize.js'
 import cron from 'node-cron'
-import updateFreeRequests from "./helpers/updateFreeRequests.js";
-import sendMessagesToAllUsers from "./helpers/sendMessagesToAllUsers.js";
-import draw from "./commands/draw.js";
-import {drawScene} from "./scene/draw.js";
-import profile from "./commands/profile.js";
-import subscribe from "./commands/subscribe.js";
-import isSubMiddleware from "./middlewares/isSubMiddleware.js";
-import isActiveSubscriptionMiddleware from "./middlewares/isActiveSubscriptionMiddleware.js";
-import {Subscribers} from "./models/Subscribers.js";
-import {User} from "./models/User.js";
-import {circleScene} from "./scene/circle.js";
-import express from "express";
-import * as ngrok from "ngrok";
+import updateFreeRequests from './helpers/updateFreeRequests.js'
+import sendMessagesToAllUsers from './helpers/sendMessagesToAllUsers.js'
+import draw from './commands/draw.js'
+import { drawScene } from './scene/draw.js'
+import profile from './commands/profile.js'
+import subscribe from './commands/subscribe.js'
+import isSubMiddleware from './middlewares/isSubMiddleware.js'
+import isActiveSubscriptionMiddleware from './middlewares/isActiveSubscriptionMiddleware.js'
+import { Subscribers } from './models/Subscribers.js'
+import { User } from './models/User.js'
+import { circleScene } from './scene/circle.js'
+import express from 'express'
+import * as ngrok from 'ngrok'
 
 const bot = new Telegraf(process.env.TOKEN)
 
 const stage = new Stage([gptScene, drawScene, circleScene])
 bot.use(session())
-bot.use(stage.middleware());
-
+bot.use(stage.middleware())
 
 // bot.command('gpt', isActiveSubscriptionMiddleware, gpt)
 // bot.command('circle', isActiveSubscriptionMiddleware, circle)
 // bot.command('draw', isActiveSubscriptionMiddleware, draw)
 
+bot.command('adminSubscribe', async ctx => {
+	const tgId = ctx.from.id
+	let currentTimeMillis = Date.now()
+	let currentDate = new Date(currentTimeMillis)
 
+	let year = currentDate.getFullYear()
+	let month = ('0' + (currentDate.getMonth() + 1)).slice(-2) // добавляем ноль перед месяцем
+	let day = ('0' + currentDate.getDate()).slice(-2) // добавляем ноль перед днем
+	let hours = ('0' + currentDate.getHours()).slice(-2) // добавляем ноль перед часами
+	let minutes = ('0' + currentDate.getMinutes()).slice(-2) // добавляем ноль перед минутами
+	let seconds = ('0' + currentDate.getSeconds()).slice(-2) // добавляем ноль перед секундами
 
-bot.command('adminSubscribe', async (ctx) => {
-  const tgId = ctx.from.id
-  let currentTimeMillis = Date.now();
-  let currentDate = new Date(currentTimeMillis);
+	let formattedDate = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`
 
-  let year = currentDate.getFullYear();
-  let month = ('0' + (currentDate.getMonth() + 1)).slice(-2); // добавляем ноль перед месяцем
-  let day = ('0' + currentDate.getDate()).slice(-2); // добавляем ноль перед днем
-  let hours = ('0' + currentDate.getHours()).slice(-2); // добавляем ноль перед часами
-  let minutes = ('0' + currentDate.getMinutes()).slice(-2); // добавляем ноль перед минутами
-  let seconds = ('0' + currentDate.getSeconds()).slice(-2); // добавляем ноль перед секундами
-
-  let formattedDate = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
-
-  await Subscribers.update({type: 4, active: 1, start: formattedDate}, {where: {tgId: tgId}})
-  ctx.reply('подписка выдана')
+	await Subscribers.update(
+		{ type: 4, active: 1, start: formattedDate },
+		{ where: { tgId: tgId } }
+	)
+	ctx.reply('подписка выдана')
 })
 
-
-sequelize.sync();
+sequelize.sync()
 
 cron.schedule('0 0 * * *', async () => {
-  await updateFreeRequests();
-  await sendMessagesToAllUsers(bot)
-});
+	await updateFreeRequests()
+	await sendMessagesToAllUsers(bot)
+})
 
 cron.schedule('0 0 1 * *', async () => {
-  const users = await User.findAll();
-  for (const user of users) {
-    await user.update({ dalleRequestsCount: 0 });
-  }
-});
+	const users = await User.findAll()
+	for (const user of users) {
+		await user.update({ dalleRequestsCount: 0 })
+	}
+})
 
 bot.use(start, restart, profile, subscribe, gpt, circle, draw)
-bot.use(isSubMiddleware());
+bot.use(isSubMiddleware())
 
-const app = express();
-app.post('/webhook', (req, res) => {
-  try {
-    console.log(req.body)
-    console.log(req.headers)
-    bot.telegram.sendMessage(740207590, `Платеж успешно выполнен.`)
-    res.status(200).send('OK');
-    // console.log(er)
-  } catch (e) {console.log(e)}
-});
-import { WalletPaySDK } from 'wallet-pay-sdk';
-import {ECurrencyCode} from "wallet-pay-sdk/lib/src/types.js";
+import bodyParser from 'body-parser'
+
+const app = express()
+import { WalletPaySDK } from 'wallet-pay-sdk'
 const wp = new WalletPaySDK({
-  apiKey: 'M4DwQAaQ7THbiAoaqzf25ee24oQygNF4ytJ0',
-  timeoutSeconds: 60 * 60 * 3, // Default value = Order TTL, if the order is not paid within the timeout period
-});
+	apiKey: 'M4DwQAaQ7THbiAoaqzf25ee24oQygNF4ytJ0',
+	timeoutSeconds: 60 * 60 * 3, // Default value = Order TTL, if the order is not paid within the timeout period
+})
+app.use(bodyParser.json())
+app.post('/webhook', async (req, res) => {
+	try {
+		const payload = req.body[0].payload
+		console.log(payload)
+		bot.telegram.sendMessage(
+			740207590,
+			`Платеж на сумму ${payload.orderAmount.amount} ${payload.orderAmount.currencyCode} (${payload.selectedPaymentOption.amount.amount} ${payload.selectedPaymentOption.amount.currencyCode}) от пользователя ${payload.customData} успешно выполнен.`
+		)
+		bot.telegram.sendMessage(
+			Number(payload.customData),
+			`Платеж на сумму ${payload.orderAmount.amount} ${payload.orderAmount.currencyCode} успешно выполнен.`
+		)
+		res.status(200).send('OK')
+		// console.log(er)
+	} catch (e) {
+		console.log(e)
+	}
+})
 
-import crypto from 'crypto'
+bot.command('pay', async ctx => {
+	await ctx.reply('payment')
+	const newOrder = {
+		amount: {
+			currencyCode: ECurrencyCode.USDT,
+			amount: '0.01',
+		},
+		description: 'My first order', // Description of the order
+		returnUrl: 'https://example.com', //  Url to redirect after paying order
+		failReturnUrl: 'https://example.com', // Url to redirect after unsuccessful order completion (expiration/cancelation/etc)
+		externalId: crypto.randomUUID(),
+		// timeoutSeconds: 200000; // If you want, you can override the value of the "timeoutSeconds" variable here
+		customerTelegramUserId: ctx.from.id, // The customer's telegram id (User_id)
+	}
 
-bot.command('pay', async (ctx) => {
-  await ctx.reply('payment')
-  const newOrder = {
-    amount: {
-      currencyCode: ECurrencyCode.USDT,
-      amount: '0.01',
-    },
-    description: 'My first order', // Description of the order
-    returnUrl: 'https://example.com', //  Url to redirect after paying order
-    failReturnUrl: 'https://example.com', // Url to redirect after unsuccessful order completion (expiration/cancelation/etc)
-    externalId: crypto.randomUUID(),
-    // timeoutSeconds: 200000; // If you want, you can override the value of the "timeoutSeconds" variable here
-    customerTelegramUserId: ctx.from.id, // The customer's telegram id (User_id)
-  }
-
-  const result = await wp.createOrder(newOrder).then(async res => {
-    console.log(res)
-    await ctx.reply(`payment`, Markup.inlineKeyboard([
-      Markup.button.url('pay', res.response.data.payLink)
-    ]))
-  })
+	const result = await wp.createOrder(newOrder).then(async res => {
+		console.log(res)
+		await ctx.reply(
+			`payment`,
+			Markup.inlineKeyboard([
+				Markup.button.url('pay', res.response.data.payLink),
+			])
+		)
+	})
 })
 
 // Запуск сервера на порте 3000
 app.listen(5124, () => {
-  console.log('Сервер запущен на порту 5124');
-  // ngrok.connect(5124).then(ngrokUrl => {
-  //   console.log(`ngrok ${ngrokUrl}`)
-  // });
-});
+	console.log('Сервер запущен на порту 5124')
+	// ngrok.connect(5124).then(ngrokUrl => {
+	//   console.log(`ngrok ${ngrokUrl}`)
+	// });
+})
 
-bot.launch();
-
-
+bot.launch()
--- commands/subscribe.js
+++ commands/subscribe.js
@@ -1,148 +1,227 @@
-import {Composer} from "telegraf";
-import {sendErrorMessage} from "../helpers/errorAndTokensExpired.js";
-import {subscribeMessage} from "../messages/subscribe.js";
-import {User} from "../models/User.js";
+import { Composer } from 'telegraf'
+import { sendErrorMessage } from '../helpers/errorAndTokensExpired.js'
+import { subscribeMessage } from '../messages/subscribe.js'
+import { User } from '../models/User.js'
 import 'dotenv/config'
-import {Subscribers} from "../models/Subscribers.js";
+import { Subscribers } from '../models/Subscribers.js'
+import { WalletPaySDK } from 'wallet-pay-sdk'
+import { ECurrencyCode } from 'wallet-pay-sdk/lib/src/types.js'
+import crypto from 'crypto'
+import { Markup } from 'telegraf'
+const wp = new WalletPaySDK({
+	apiKey: 'M4DwQAaQ7THbiAoaqzf25ee24oQygNF4ytJ0',
+	timeoutSeconds: 60 * 60 * 3, // Default value = Order TTL, if the order is not paid within the timeout period
+})
 
 const composer = new Composer()
 
+composer.action('sub 1', async ctx => {
+	// const invoice = {
+	//   provider_token: process.env.PAYMENTS_KEY, // Replace with your provider token
+	//   start_parameter: 'invoice123',
+	//   title: 'GPT 3.5',
+	//   description: 'Безлимитное количество запросов GPT 3.5 - 350₽/месяц',
+	//   currency: 'RUB',
+	//   prices: [
+	//     { label: 'Product 1', amount: 35000 }, // Amount is in cents
+	//   ],
+	//   payload: 'custom_payload',
+	// };
 
-composer.action('sub 1', async (ctx) => {
-  const invoice = {
-    provider_token: process.env.PAYMENTS_KEY, // Replace with your provider token
-    start_parameter: 'invoice123',
-    title: 'GPT 3.5',
-    description: 'Безлимитное количество запросов GPT 3.5 - 350₽/месяц',
-    currency: 'RUB',
-    prices: [
-      { label: 'Product 1', amount: 35000 }, // Amount is in cents
-    ],
-    payload: 'custom_payload',
-  };
+	// ctx.replyWithInvoice(invoice);
+	const newOrder = {
+		amount: {
+			currencyCode: ECurrencyCode.RUB,
+			amount: '0.1',
+		},
+		description: 'Безлимитное количество запросов GPT 3.5 - 350₽/месяц', // Description of the order
+		externalId: crypto.randomUUID(),
+		customData: `${ctx.from.id}`,
+		customerTelegramUserId: ctx.from.id, // The customer's telegram id (User_id)
+	}
 
-  ctx.replyWithInvoice(invoice);
-  ctx.type = 1;
+	const result = await wp.createOrder(newOrder).then(async res => {
+		console.log(res)
+		await ctx.reply(
+			`Нажмите на кнопку ниже, чтобы оплатить 💸`,
+			Markup.inlineKeyboard([
+				Markup.button.url('Оплатить', res.response.data.payLink),
+			])
+		)
+	})
+	ctx.type = 1
 })
 
-composer.action('sub 2', async (ctx) => {
-  const invoice = {
-    provider_token: process.env.PAYMENTS_KEY, // Replace with your provider token
-    start_parameter: 'invoice1234',
-    title: 'Безлим GPT 3.5 + GPT 4',
-    description: 'Безлим GPT 3.5 + GPT 4 - 550₽/месяц',
-    currency: 'RUB',
-    prices: [
-      { label: 'Product 2', amount: 55000 }, // Amount is in cents
-    ],
-    payload: 'custom_payload',
-  };
+composer.action('sub 2', async ctx => {
+	const newOrder = {
+		amount: {
+			currencyCode: ECurrencyCode.RUB,
+			amount: '10',
+		},
+		description: 'Безлимитное GPT 3.5 + GPT 4',
+		externalId: crypto.randomUUID(),
+		customerTelegramUserId: ctx.from.id, // The customer's telegram id (User_id)
+	}
 
-  ctx.replyWithInvoice(invoice);
-  ctx.type = 2;
+	const result = await wp.createOrder(newOrder).then(async res => {
+		console.log(res)
+		await ctx.reply(
+			`Нажмите на кнопку ниже, чтобы оплатить 💸`,
+			Markup.inlineKeyboard([
+				Markup.button.url('Оплатить', res.response.data.payLink),
+			])
+		)
+	})
+	ctx.type = 2
 })
 
-composer.action('sub 3', async (ctx) => {
-  const invoice = {
-    provider_token: process.env.PAYMENTS_KEY, // Replace with your provider token
-    start_parameter: 'invoice12345',
-    title: 'Midjourney + DALL-E 3',
-    description: 'Midjourney + DALL-E 3 - 1250₽/месяц (лимит 100 запросов)',
-    currency: 'RUB',
-    prices: [
-      { label: 'Product 3', amount: 125000 }, // Amount is in cents
-    ],
-    payload: 'custom_payload',
-  };
+composer.action('sub 3', async ctx => {
+	// const invoice = {
+	// 	provider_token: process.env.PAYMENTS_KEY, // Replace with your provider token
+	// 	start_parameter: 'invoice12345',
+	// 	title: 'Midjourney + DALL-E 3',
+	// 	description: 'Midjourney + DALL-E 3 - 1250₽/месяц (лимит 100 запросов)',
+	// 	currency: 'RUB',
+	// 	prices: [
+	// 		{ label: 'Product 3', amount: 125000 }, // Amount is in cents
+	// 	],
+	// 	payload: 'custom_payload',
+	// }
 
-  ctx.replyWithInvoice(invoice);
-  ctx.type = 3;
+	// ctx.replyWithInvoice(invoice)
+	const newOrder = {
+		amount: {
+			currencyCode: ECurrencyCode.RUB,
+			amount: '10',
+		},
+		description: 'Midjourney + DALL-E 3 100 запросов', // Description of the order
+		externalId: crypto.randomUUID(),
+		customerTelegramUserId: ctx.from.id, // The customer's telegram id (User_id)
+	}
+
+	const result = await wp.createOrder(newOrder).then(async res => {
+		console.log(res)
+		await ctx.reply(
+			`Нажмите на кнопку ниже, чтобы оплатить 💸`,
+			Markup.inlineKeyboard([
+				Markup.button.url('Оплатить', res.response.data.payLink),
+			])
+		)
+	})
+	ctx.type = 3
 })
 
-composer.action('sub 4', async (ctx) => {
-  const invoice = {
-    provider_token: process.env.PAYMENTS_KEY, // Replace with your provider token
-    start_parameter: 'invoice12367',
-    title: 'GPT 3.5 + GPT 4 + Midjourney + DALL-E 3 ',
-    description: 'GPT 3.5 + GPT 4 + Midjourney + DALL-E 3  - 2500₽/месяц (GPT безлимит, Midjourney + DALL-E 3 - 150 картинок)',
-    currency: 'RUB',
-    prices: [
-      { label: 'Product 4', amount: 250000 }, // Amount is in cents
-    ],
-    payload: 'custom_payload',
-  };
+composer.action('sub 4', async ctx => {
+	// const invoice = {
+	// 	provider_token: process.env.PAYMENTS_KEY, // Replace with your provider token
+	// 	start_parameter: 'invoice12367',
+	// 	title: 'GPT 3.5 + GPT 4 + Midjourney + DALL-E 3 ',
+	// 	description:
+	// 		'GPT 3.5 + GPT 4 + Midjourney + DALL-E 3  - 2500₽/месяц (GPT безлимит, Midjourney + DALL-E 3 - 150 картинок)',
+	// 	currency: 'RUB',
+	// 	prices: [
+	// 		{ label: 'Product 4', amount: 250000 }, // Amount is in cents
+	// 	],
+	// 	payload: 'custom_payload',
+	// }
 
-  ctx.replyWithInvoice(invoice);
-  ctx.type = 4;
+	// ctx.replyWithInvoice(invoice)
+	const newOrder = {
+		amount: {
+			currencyCode: ECurrencyCode.RUB,
+			amount: '10',
+		},
+		description:
+			'GPT 3.5 + GPT 4 + Midjourney + DALL-E 3  (безлимит, 150 картинок)', // Description of the order
+		externalId: crypto.randomUUID(),
+		customerTelegramUserId: ctx.from.id, // The customer's telegram id (User_id)
+	}
+
+	const result = await wp.createOrder(newOrder).then(async res => {
+		console.log(res)
+		await ctx.reply(
+			`Нажмите на кнопку ниже, чтобы оплатить 💸`,
+			Markup.inlineKeyboard([
+				Markup.button.url('Оплатить', res.response.data.payLink),
+			])
+		)
+	})
+	ctx.type = 4
 })
 
-composer.on('pre_checkout_query', async (ctx) => {
-  ctx.answerPreCheckoutQuery(true);
+// composer.on('pre_checkout_query', async ctx => {
+// 	ctx.answerPreCheckoutQuery(true)
+// })
 
-})
+// composer.on('successful_payment', async ctx => {
+// 	const tgId = ctx.from.id
+// 	let currentTimeMillis = Date.now()
+// 	let currentDate = new Date(currentTimeMillis)
 
-composer.on('successful_payment', async (ctx) => {
-  const tgId = ctx.from.id;
-  let currentTimeMillis = Date.now();
-  let currentDate = new Date(currentTimeMillis);
+// 	let year = currentDate.getFullYear()
+// 	let month = ('0' + (currentDate.getMonth() + 1)).slice(-2) // добавляем ноль перед месяцем
+// 	let day = ('0' + currentDate.getDate()).slice(-2) // добавляем ноль перед днем
+// 	let hours = ('0' + currentDate.getHours()).slice(-2) // добавляем ноль перед часами
+// 	let minutes = ('0' + currentDate.getMinutes()).slice(-2) // добавляем ноль перед минутами
+// 	let seconds = ('0' + currentDate.getSeconds()).slice(-2) // добавляем ноль перед секундами
 
-  let year = currentDate.getFullYear();
-  let month = ('0' + (currentDate.getMonth() + 1)).slice(-2); // добавляем ноль перед месяцем
-  let day = ('0' + currentDate.getDate()).slice(-2); // добавляем ноль перед днем
-  let hours = ('0' + currentDate.getHours()).slice(-2); // добавляем ноль перед часами
-  let minutes = ('0' + currentDate.getMinutes()).slice(-2); // добавляем ноль перед минутами
-  let seconds = ('0' + currentDate.getSeconds()).slice(-2); // добавляем ноль перед секундами
+// 	let formattedDate = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`
+// 	await Subscribers.update(
+// 		{ type: ctx.type, start: formattedDate },
+// 		{
+// 			where: { tgId: tgId },
+// 		}
+// 	)
+// 	ctx.reply('Оплата прошла успешно')
+// })
 
-  let formattedDate = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
-  await Subscribers.update({type: ctx.type, start: formattedDate}, {
-    where: {tgId: tgId}
-  })
-  ctx.reply('Оплата прошла успешно')
-})
+composer.command('subscribe', async ctx => {
+	const chatId = ctx.from.id
+	const user = await User.findOne({
+		where: {
+			tgId: chatId,
+		},
+	})
+	if (!user) {
+		return sendErrorMessage(ctx)
+	}
 
-
-
-composer.command('subscribe', async (ctx) => {
-  const chatId = ctx.from.id;
-  const user = await User.findOne({
-    where: {
-      tgId: chatId
-    }
-  })
-  if (!user) {
-    return sendErrorMessage(ctx)
-  }
-
-  return ctx
-    .reply(subscribeMessage, {
-        parse_mode: "html",
-        reply_markup: {
-          inline_keyboard: [
-            [{
-              text: "Безлим. GPT 3.5 🌑",
-              callback_data: "sub 1",
-            }],
-            [{
-              text: "Безлим GPT 3.5 + GPT 4 🌒",
-              callback_data: "sub 2",
-            }],
-            [{
-              text: "Midjourney + DALL-E 3 🌓",
-              callback_data: "sub 3",
-            }],
-            [{
-              text: "GPT 3.5 + GPT 4 + Midjourney + DALL-E 3 🌔",
-              callback_data: "sub 4",
-            },
-            ],
-          ],
-        },
-        disable_web_page_preview: true,
-      }
-    )
-    .catch((err) => {
-      console.log(err);
-    });
+	return ctx
+		.reply(subscribeMessage, {
+			parse_mode: 'html',
+			reply_markup: {
+				inline_keyboard: [
+					[
+						{
+							text: 'Безлим. GPT 3.5 🌑',
+							callback_data: 'sub 1',
+						},
+					],
+					[
+						{
+							text: 'Безлим GPT 3.5 + GPT 4 🌒',
+							callback_data: 'sub 2',
+						},
+					],
+					[
+						{
+							text: 'Midjourney + DALL-E 3 🌓',
+							callback_data: 'sub 3',
+						},
+					],
+					[
+						{
+							text: 'GPT 3.5 + GPT 4 + Midjourney + DALL-E 3 🌔',
+							callback_data: 'sub 4',
+						},
+					],
+				],
+			},
+			disable_web_page_preview: true,
+		})
+		.catch(err => {
+			console.log(err)
+		})
 })
 
 export default composer
